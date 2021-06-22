6556f6308c48e741111e29f27e1de049ca4c734f
harizMunawar <munawarhariz@gmail.com>
2021-06-22 14:20:46 +0700

    Document merchant's CRUD api

--- core/merchant_views.py
+++ core/merchant_views.py
@@ -8,20 +8,44 @@ from core.serializers import CreateUserSerializer, MerchantSerializer
 from core.models import Merchant
 from core.permissions import IsStaffOrReadOnly
 
+from drf_yasg.utils import swagger_auto_schema
 
 class MerchantList(views.APIView):
-    """
-    List all merchants or create a new one
-    """
-
     permission_classes = [IsStaffOrReadOnly,]
 
+    @swagger_auto_schema(
+        responses={
+            200: MerchantSerializer(),
+            204: "No Content"
+        }
+    )
     def get(self, request):
+        """
+        Merchant's List
+
+        List all merchants, if 0 merchant exists return 204.
+        """
         merchant = Merchant.objects.all()
         serializer = MerchantSerializer(merchant, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        request_body=CreateUserSerializer(),
+        responses={
+            201: MerchantSerializer(),
+            400: "Bad Request",
+            403: "Staff Account is Required",
+        }
+    )
     def post(self, request):
+        """
+        Create Merchant
+
+        Username field must be unique.
+        Return 201 with merchant serializer if merchant created successfully.
+        Return 400 if request is invalid.
+        Return 403 if request is done by a non-staff account.
+        """
         serializer = CreateUserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -43,11 +67,49 @@ class MerchantDetail(
     serializer_class = MerchantSerializer
     permission_classes = [IsStaffOrReadOnly,]
 
+    @swagger_auto_schema(
+        responses={
+            200: MerchantSerializer(),
+            404: "Invalid Merchant's ID or Not Found"
+        }
+    )
     def get(self, request, *args, **kwargs):
+        """
+        Merchant's Detail
+
+        Return 200 with merchant serializer if merchant exists.
+        Return 404 if no merchant found with that ID.
+        """
         return self.retrieve(request, *args, **kwargs)
 
+    @swagger_auto_schema(
+        responses={
+            200: MerchantSerializer(),
+            400: "Bad Request",
+            404: "Invalid Merchant's ID or Not Found"
+        }
+    )
     def put(self, request, *args, **kwargs):
+        """
+        Merchant's Update
+
+        Return 200 with merchant serializer if merchant exists and updated successfully.
+        Return 400 if request is invalid.
+        Return 404 if no merchant found with that ID.
+        """
         return self.update(request, *args, **kwargs)
 
+    @swagger_auto_schema(
+        responses={
+            204: "No Content",
+            404: "Invalid Merchant's ID or Not Found"
+        }
+    )
     def delete(self, request, *args, **kwargs):
+        """
+        Merchant's Delete
+
+        Return 204 with no content if merchant exists and deleted successfully.
+        Return 404 if no merchant found with that ID.
+        """
         return self.destroy(request, *args, **kwargs)
--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from core import views, merchant_views, customer_views
 
 urlpatterns = [
     path('merchants/', merchant_views.MerchantList.as_view()),
-    path('merchants/<int:id>/', merchant_views.MerchantDetail.as_view()),
+    path('merchants/<int:pk>/', merchant_views.MerchantDetail.as_view()),
     path('customers/', customer_views.CustomerList.as_view()),
-    path('customers/<int:id>/', customer_views.CustomerDetail.as_view()),
+    path('customers/<int:pk>/', customer_views.CustomerDetail.as_view()),
 ]
